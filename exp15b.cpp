c528accde429a4a13fb61869165718f60560cb74
AaryaJirwankar <aaryajirwankar@gmail.com>
2024-11-08 12:00:24 +0530

    exp15b.cpp

---
+++ exp15b.cpp
@@ -0,0 +1,24 @@
+//Aarya Jirwankar
+//23070123161
+
+#include <iostream>
+using namespace std;
+int add(int n)
+{
+    if (n<=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n+add(n-1));
+    }
+}
+int main()
+{
+    int x,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    x= add(n);
+    cout<<x;
+}
